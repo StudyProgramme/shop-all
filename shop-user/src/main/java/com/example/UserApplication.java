ce7033d437712839b601f280dc2eff1c47382886
xx <2023170703@qq.com>
2022-03-21 20:38:36 +0800

    Initial commit

---
+++ shop-user/src/main/java/com/example/UserApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author x
+ * @date 2022年03月21日 20:25
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args){
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
